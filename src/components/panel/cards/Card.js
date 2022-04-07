7835c6e8a60c5255f2bd073de61e6b1134beac51
AshforthM <ashforth.m@gmail.com>
2022-04-07 11:55:05 -0600

    update card styles

--- src/components/panel/cards/Card.js
+++ src/components/panel/cards/Card.js
@@ -15,16 +15,6 @@ export default function Card({product, ...props}) {
   );
 }
 
-const ProductCardStyles = styled.aside`
-  height: 520px;
-  max-width: 360px;
-  align-self: stretch;
-  padding: 1rem 2rem;
-  margin: 0.5rem;
-  background-color: #edece9;
-  border-radius: 8px;
-`;
-
 const ProductImage = styled.div`
   padding: 0.25rem 0 1rem 0;
   width: 300px;
@@ -36,11 +26,43 @@ const ProductImage = styled.div`
 `;
 
 const ProductName = styled.h2`
+  position: relative;
   font-size: 2.25rem;
   font-weight: 700;
-  line-height: 2.5rem;
+  line-height: 2.3rem;
   letter-spacing: -0.5px;
   word-wrap: break-word;
+  padding-bottom:3px;
+
+  &:after{
+    position: absolute;
+    content: "";
+    background: rgb(251, 174, 45);
+    height: 3px;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    transform: scaleX(0);
+    transition: transform 0.5s ease;
+  }
+`;
+
+const ProductCardStyles = styled.aside`
+  height: 520px;
+  max-width: 360px;
+  align-self: stretch;
+  padding: 1rem 2rem;
+  margin: 0.5rem;
+  background-color: #edece9;
+  border-radius: 8px;
+  transition: transform 0.5s ease;
+
+  &:hover{
+    transform: scale(1.025);
+    ${ProductName}::after{
+      transform: scaleX(1);
+    }
+  }
 `;
 
 const ProductPrice = styled.p`
